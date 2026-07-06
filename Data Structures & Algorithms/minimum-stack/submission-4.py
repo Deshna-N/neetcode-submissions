1d13e07fbe1bc440830c8b3f192beb5071dab889
Deshna-N <d35hnanaruka@gmail.com>
2026-07-06 10:47:01 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,40 @@
+class MinStack:
+
+    def __init__(self): 
+        self.stack = []
+        self.newstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.newstack:
+            self.newstack.append(val)
+        else:    
+            if self.newstack[-1] < val:
+                self.newstack.append(self.newstack[-1])
+            elif (self.newstack[-1] > val) or (self.newstack[-1] == val):
+                self.newstack.append(val)
+        
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        self.newstack.pop()
+    
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        ## so stack has 5, if newstack empty then push
+        ## stack now has 5,2 and compare newstack.top with 2, if smaller than pop and add 2 else continue
+        ## and when popping from stack then pop from newstack which always got 1 val
+        return self.newstack[-1]
+            
+
+
+
+
+
+        # self.small = float('inf')
+        # for i in self.stack:
+        #     self.small = min(i, self.small)
+        # return self.small
+        
